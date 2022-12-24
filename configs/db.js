462a33f95fe3f3593a3f1cfea81f9d051ccadacc
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-24 15:27:52 +0500

    some updaets

--- configs/db.js
+++ configs/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize')
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'postgres',
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.NODE_ENV === 'production' ? 25060 : 5432,
     logging: false,
 })
 
