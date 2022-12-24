df76e48d25cabd6315b5458844422d10a0fdf615
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-24 15:38:14 +0500

    bug fixes db

--- configs/db.js
+++ configs/db.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     port: process.env.NODE_ENV === 'production' ? 25060 : 5432,
     logging: false,
+    ssl: true,
 })
 
 module.exports = sequelize
