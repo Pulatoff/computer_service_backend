23241688833800f989a28ae85bdacf385a38c02e
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-24 15:47:16 +0500

    some updatse

--- configs/db.js
+++ configs/db.js
@@ -4,6 +4,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     dialect: 'postgres',
     host: process.env.DB_HOST,
     port: process.env.NODE_ENV === 'production' ? 25060 : 5432,
+    native: true,
     logging: false,
     ssl: true,
 })
