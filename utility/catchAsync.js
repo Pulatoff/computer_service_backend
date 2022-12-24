7217d5a010e69ed1212f3a384e4a2b38161f70e0
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-24 11:55:38 +0500

    some updates

--- utility/catchAsync.js
+++ utility/catchAsync.js
@@ -1,10 +1,10 @@
-const AppError = require("./appError");
+const AppError = require('./appError')
 
 const catchAsync = (fn) => {
-  const func = (req, res, next) => {
-    fn(req, res, next).catch((err) => next(new AppError(err.message, 500)));
-  };
-  return func;
-};
+    const func = (req, res, next) => {
+        fn(req, res, next).catch((err) => next(new AppError(err.message, 500)))
+    }
+    return func
+}
 
-module.exports = catchAsync;
+module.exports = catchAsync
