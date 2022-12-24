a41dc46a0174a2817d66a66c69cb1ea99d34c93e
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-24 16:09:29 +0500

    some updates

--- configs/db.js
+++ configs/db.js
@@ -4,8 +4,12 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     dialect: 'postgres',
     host: process.env.DB_HOST,
     port: process.env.NODE_ENV === 'production' ? 25060 : 5432,
-    ssl: true,
     logging: false,
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
 })
 
 module.exports = sequelize
