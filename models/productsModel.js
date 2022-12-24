f0365541c55d80fb60deb8e23dd600369f7c95f0
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-24 11:30:55 +0500

    some updates

--- models/productsModel.js
+++ models/productsModel.js
@@ -9,17 +9,7 @@ const Product = sequelize.define(
         id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
         name: { type: DataTypes.STRING, allowNull: false, validate: { min: 3, max: 250 } },
         image_main: { type: DataTypes.STRING, allowNull: false },
-        saleId: { type: DataTypes.UUID, allowNull: true, references: { model: 'sales', key: 'id' } }, //boshqa table
-        rewiesId: { type: DataTypes.UUID, allowNull: true, references: { model: 'reviews', key: 'id' } }, //boshqa table
-        productDetailId: { type: DataTypes.UUID, allowNull: false, references: { model: 'productDetails', key: 'id' } }, //boshqa table
-        categoryId: { type: DataTypes.UUID, allowNull: false, references: { model: 'categories', key: 'id' } },
-        categoryLittleId: {
-            type: DataTypes.UUID,
-            allowNull: false,
-            references: { model: 'categoryLittles', key: 'id' },
-        },
         views: { type: DataTypes.INTEGER, defaultValue: 1 },
-        brandId: { type: DataTypes.UUID, allowNull: false, references: { model: 'brands', key: 'id' } },
     },
     { timestamps: true, createdAt: true, updatedAt: false }
 )
--- models/reviewsModel.js
+++ models/reviewsModel.js
@@ -1,17 +1,18 @@
 const sequelize = require('../configs/db')
+const { DataTypes } = require('sequelize')
+const Product = require('./productsModel')
 
-const Reviews = sequelize.define(
+const Review = sequelize.define(
     'reviews',
     {
         id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
         body: { type: DataTypes.TEXT, allowNull: false },
         rating: { type: DataTypes.INTEGER, allowNull: false, validate: { min: 0, max: 5 } },
     },
-    {
-        timestamps: true,
-        createdAt: true,
-        updatedAt: false,
-    }
+    { timestamps: true, createdAt: true, updatedAt: false }
 )
 
-module.exports = Reviews
+Product.hasMany(Review)
+Review.belongsTo(Product)
+
+module.exports = Review
