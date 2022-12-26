b7c04f88fb28e53711d871800799d3091a9b2626
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-26 22:13:31 +0500

    some updats

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,9 @@ const User = require('../models/userModel')
 const catchAsync = require('../utility/catchAsync')
 const response = require('../utility/response')
 
-exports.getAllUsers = catchAsync(async (req, res) => {
+exports.getAllUsers = catchAsync(async (req, res, next) => {
     const users = await User.findAll({ attributes: { exclude: ['password'] } })
-    response(res, { data: users }, 200, `You are successfullt get ${users.length}`, users.length)
+    response(res, { data: users }, 200, `You are successfully get ${users.length}`, users.length)
 })
 
 exports.createUser = catchAsync(async (req, res, next) => {
@@ -15,31 +15,28 @@ exports.createUser = catchAsync(async (req, res, next) => {
     response(res, { user }, 201, `You created new consumer by id: ${user.id}`)
 })
 
-exports.deleteUser = catchAsync(async (req, res) => {
+exports.deleteUser = catchAsync(async (req, res, next) => {
     const id = req.params.id
     await User.destroy({ where: { id } })
-    res.status(200).json({ data: 'success' })
+    response(res, '', 206, 'user successfully deleted')
 })
 
-exports.getOneUser = catchAsync(async (req, res) => {
+exports.getOneUser = catchAsync(async (req, res, next) => {
     const id = req.params.id
     const user = await User.findOne({ where: { id }, attributes: { exclude: ['password'] } })
     response(res, { user }, 200, 'You successfully get user')
 })
 
-exports.updateUser = catchAsync(async (req, res) => {
+exports.updateUser = catchAsync(async (req, res, next) => {
     const id = req.params.id
+    const { username, activ, role, email } = req.body
     const user = await User.findOne({ where: { id } })
 
-    user.first_name = req.body.first_name || user.first_name
-    user.last_name = req.body.last_name || user.last_name
-    user.username = req.body.username || user.username
-    user.email = req.body.email || user.email
-    user.phone = req.body.phone || user.phone
-    user.password = req.body.password || user.password
-    user.locationId = req.body.locationId || user.locationId
+    user.username = username || user.username
+    user.email = email || user.email
+    user.activ = activ || user.activ
+    user.role = role || user.role
+    await user.save()
 
-    const newUser = await user.save()
-
-    res.status(200).json({ data: newUser })
+    response(res, { user }, 200, 'you successfully update user')
 })
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -10,6 +10,10 @@ router.route('/signup').post(auth.signUp)
 router.route('/signin').post(auth.login)
 router.route('/self').get(auth.protect, auth.userSelf)
 router.route('/logout').get(auth.protect, auth.logout)
-router.route('/:id').get(auth.protect, auth.role(['admin']), controller.getOneUser)
+router
+    .route('/:id')
+    .get(auth.protect, auth.role(['admin']), controller.getOneUser)
+    .patch(auth.protect, auth.role(['admin']), controller.updateUser)
+    .delete(auth.protect, auth.role(['admin']), controller.deleteUser)
 
 module.exports = router
