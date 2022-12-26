9e891bd9dbd2fa6ffcbf5a57aff3e6233601c707
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-26 20:49:02 +0500

    some updates

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,4 +5,5 @@ router.route('/signup').post(auth.signUp)
 router.route('/signin').post(auth.login)
 router.route('/self').get(auth.protect, auth.userSelf)
 router.route('/logout').get(auth.protect, auth.logout)
+
 module.exports = router
