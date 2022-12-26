25f62e90801c9ea3d1909c813f5ab4abadccd7e0
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-26 20:20:15 +0500

    some updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,67 +1,50 @@
-const db = require("../configs/db");
+const db = require('../configs/db')
 
-const User = db.users;
+const User = db.users
 
-const Location = db.locations;
-const cli = require("cli-color");
-const catchAsync = require("../utility/catchAsync");
+const Location = db.locations
+const cli = require('cli-color')
+const catchAsync = require('../utility/catchAsync')
 
 const getAll = catchAsync(async (req, res) => {
-  const users = await User.findAll({ include: Location }); // required:true
+    const users = await User.findAll()
 
-  res.status(200).json({
-    data: users,
-  });
-});
+    res.status(200).json({ data: users })
+})
 
 const add = catchAsync(async (req, res) => {
-  const user = await User.create(req.body);
-  res.status(200).json({
-    data: user,
-  });
-});
+    const user = await User.create(req.body)
+    res.status(200).json({ data: user })
+})
 
 const delete1 = catchAsync(async (req, res) => {
-  await User.destroy({ where: { id: req.params.id } });
-  res.status(200).json({
-    data: "success",
-  });
-});
+    await User.destroy({ where: { id: req.params.id } })
+    res.status(200).json({ data: 'success' })
+})
 const getOne = catchAsync(async (req, res) => {
-  const user = await User.findOne({
-    where: { id: req.params.id },
-    include: { model: Location },
-  });
-  res.status(200).json({
-    data: user,
-  });
-});
+    const user = await User.findOne({ where: { id: req.params.id }, include: { model: Location } })
+    res.status(200).json({ data: user })
+})
 const update = catchAsync(async (req, res) => {
-  try {
-    const user = await User.findOne({ where: { id: req.params.id } });
+    const user = await User.findOne({ where: { id: req.params.id } })
 
-    user.first_name = req.body.first_name || user.first_name;
-    user.last_name = req.body.last_name || user.last_name;
-    user.username = req.body.username || user.username;
-    user.email = req.body.email || user.email;
-    user.phone = req.body.phone || user.phone;
-    user.password = req.body.password || user.password;
-    user.locationId = req.body.locationId || user.locationId;
+    user.first_name = req.body.first_name || user.first_name
+    user.last_name = req.body.last_name || user.last_name
+    user.username = req.body.username || user.username
+    user.email = req.body.email || user.email
+    user.phone = req.body.phone || user.phone
+    user.password = req.body.password || user.password
+    user.locationId = req.body.locationId || user.locationId
 
-    const newUser = await user.save();
+    const newUser = await user.save()
 
-    res.status(200).json({
-      data: newUser,
-    });
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+    res.status(200).json({ data: newUser })
+})
 
 module.exports = {
-  add,
-  getAll,
-  delete1,
-  getOne,
-  update,
-};
+    add,
+    getAll,
+    delete1,
+    getOne,
+    update,
+}
