51cd1c88f02b984343e0eb39df373681c9a2115a
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-26 21:11:24 +0500

    some updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,30 @@
+// models
 const User = require('../models/userModel')
 // utils
 const catchAsync = require('../utility/catchAsync')
 const response = require('../utility/response')
-//
 
-const getAllUsers = catchAsync(async (req, res) => {
+exports.getAllUsers = catchAsync(async (req, res) => {
     const users = await User.findAll({ attributes: { exclude: ['password'] } })
     response(res, { data: users }, 200, `You are successfullt get ${users.length}`, users.length)
 })
 
-const add = catchAsync(async (req, res) => {
+exports.createUser = catchAsync(async (req, res) => {
     const user = await User.create(req.body)
     res.status(200).json({ data: user })
 })
 
-const delete1 = catchAsync(async (req, res) => {
+exports.deleteUser = catchAsync(async (req, res) => {
     await User.destroy({ where: { id: req.params.id } })
     res.status(200).json({ data: 'success' })
 })
-const getOne = catchAsync(async (req, res) => {
+
+exports.getOneUser = catchAsync(async (req, res) => {
     const user = await User.findOne({ where: { id: req.params.id }, include: { model: Location } })
     res.status(200).json({ data: user })
 })
-const update = catchAsync(async (req, res) => {
+
+exports.updateUser = catchAsync(async (req, res) => {
     const user = await User.findOne({ where: { id: req.params.id } })
 
     user.first_name = req.body.first_name || user.first_name
@@ -37,11 +39,3 @@ const update = catchAsync(async (req, res) => {
 
     res.status(200).json({ data: newUser })
 })
-
-module.exports = {
-    add,
-    getAllUsers,
-    delete1,
-    getOne,
-    update,
-}
