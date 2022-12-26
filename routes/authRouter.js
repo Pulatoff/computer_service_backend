ce67741c275befebb8fde7198bcb1a4d87d33290
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-26 22:19:29 +0500

    some updates

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,14 +6,16 @@ router
     .route('/')
     .get(auth.protect, auth.role(['admin']), controller.getAllUsers)
     .post(auth.protect, auth.role(['admin']), controller.createUser)
+
 router.route('/signup').post(auth.signUp)
 router.route('/signin').post(auth.login)
 router.route('/self').get(auth.protect, auth.userSelf)
 router.route('/logout').get(auth.protect, auth.logout)
+
 router
     .route('/:id')
     .get(auth.protect, auth.role(['admin']), controller.getOneUser)
-    .patch(auth.protect, controller.updateUser)
+    .patch(auth.protect, auth.role(['admin']), controller.updateUser)
     .delete(auth.protect, auth.role(['admin']), controller.deleteUser)
 
 module.exports = router
