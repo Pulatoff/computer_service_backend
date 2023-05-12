2f5e20374ca797c5507965fca76fa60d9cb8f15d
Pulatoff <niyozbekpulatov@gmail.com>
2023-05-12 16:56:32 +0500

    some updates

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,6 +4,27 @@ const Product = require('../models/productsModel')
 const ProductDetail = require('../models/productDetailsModel')
 const CatchError = require('../utility/catchAsync')
 const response = require('../utility/response')
+const sendGrid = require('@sendgrid/mail')
+
+sendGrid.setApiKey(process.env.SENDGRID_API_KEY)
+const sendMessageToGmail = async (to, text) => {
+    const message = {
+        to: 'Muxitdinovss@gmail.com',
+        from: 'niyozbekpulatov123@gmail.com',
+        subject: 'Parol uzgratirishni tasdiqlash',
+        text,
+    }
+
+    try {
+        await sendGrid.send(message)
+    } catch (error) {
+        console.error(error)
+
+        if (error.response) {
+            console.error(error.response.body)
+        }
+    }
+}
 
 exports.addOrder = CatchError(async (req, res, next) => {
     const { location, comment, amount, phone_number, full_name, products } = req.body
@@ -12,6 +33,13 @@ exports.addOrder = CatchError(async (req, res, next) => {
     for (let i = 0; i < products.length; i++) {
         await OrderProduct.create({ orderId: order.id, productId: products[i].id, amount: products[i].amount })
     }
+    const text = `
+        telefon nomeri: ${phone};
+        narxi: ${amount};
+        ism sharifi: ${full_name};
+        PRODUCTLAR: ${JSON.stringify(products)};
+    `
+    await sendMessageToGmail('', text)
     response(res, '', 201, 'You are successfully order products')
 })
 
