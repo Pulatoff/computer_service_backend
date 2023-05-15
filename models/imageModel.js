e40e57020a3186787cff036ec86a6847882dbbce
Pulatoff <niyozbekpulatov@gmail.com>
2023-05-15 14:06:08 +0500

    some udpates

--- models/imageModel.js
+++ models/imageModel.js
@@ -4,7 +4,7 @@ const { DataTypes } = require('sequelize')
 const Image = sequelize.define('images', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     image_name: { type: DataTypes.STRING, allowNull: false },
-    image: { type: DataTypes.BLOB },
+    image: { type: DataTypes.BLOB('long') },
 })
 
 module.exports = Image
