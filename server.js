342060d66e37d33554c81dba1af74f8d77d1b900
Pulatoff <niyozbekpulatov@gmail.com>
2023-05-15 13:28:15 +0500

    some updates

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const db = require('./configs/db')
 
 const PORT = process.env.PORT || 8000
 
-db.sync()
+async function init() {
+    try {
+        await db.sync()
+        app.listen(PORT, () => console.log(cli.blue(`Listening on PORT: ${PORT}`)))
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-app.listen(PORT, () => console.log(cli.blue(`Listening on PORT: ${PORT}`)))
+init()
