bd1e82b9c09cc80fff9f69046bf76b69f2351ce5
Pulatoff <niyozbekpulatov@gmail.com>
2023-05-29 12:51:04 +0500

    some updates

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -12,7 +12,7 @@ const sendMessageToGmail = async (to, text) => {
         to: 'Muxitdinovss@gmail.com',
         from: 'niyozbekpulatov123@gmail.com',
         subject: 'Saytdan zakaz keldi',
-        text,
+        html,
     }
 
     try {
@@ -27,18 +27,12 @@ const sendMessageToGmail = async (to, text) => {
 }
 
 exports.addOrder = CatchError(async (req, res, next) => {
-    const { location, comment, amount, phone_number, full_name, products } = req.body
+    const { location, comment, amount, phone_number, full_name, products, text } = req.body
     const order = await Order.create({ location, amount, comment, phone_number, full_name, products })
 
     for (let i = 0; i < products.length; i++) {
         await OrderProduct.create({ orderId: order.id, productId: products[i].id, amount: products[i].amount })
     }
-    const text = `
-        telefon nomeri: ${phone_number};
-        narxi: ${amount};
-        ism sharifi: ${full_name};
-        PRODUCTLAR: ${JSON.stringify(products)};
-    `
     await sendMessageToGmail('', text)
     response(res, '', 201, 'You are successfully order products')
 })
