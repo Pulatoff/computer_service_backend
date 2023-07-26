80643f604da7eb392890f07390f50c07108968d6
Pulatoff <niyozbekpulatov@gmail.com>
2023-07-26 19:47:47 +0500

    some updates

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,10 +1,18 @@
+const { callMYUZCARD } = require('../gateways/payment')
 const Order = require('../models/orderModel')
+const Transaction = require('../models/transactionsModel')
+const AppError = require('../utility/appError')
 
 // utils
 const catchError = require('../utility/catchAsync')
 const response = require('../utility/response')
 
-// exports.ConfirmPayment = catchError(async (req, res, next) => {
-//     const { otp } = req.body
-//     const 
-// })
+exports.ConfirmPayment = catchError(async (req, res, next) => {
+    const { otp, order_id } = req.body
+    const order = await Order.findByPk(order_id, { include: [{ model: Transaction }] })
+    const { result, error } = callMYUZCARD('POST', { otp, session }, '/Payment/confirmPayment')
+    if (error) {
+        next(new AppError('notugri parol', 400))
+    }
+    response(res, {}, 200, 'success')
+})
